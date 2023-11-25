af1e30ae2eceefda1c40d4b1a56e303a38d746cd
Wystan Lin <weijlin850621@gmail.com>
2023-11-26 01:17:50 +0800

    加上註解以及更新判斷條件

--- utils/linkChecker.js
+++ utils/linkChecker.js
@@ -1,6 +1,7 @@
 const ProgressBar = require("progress");
 
 async function checkLinks(page, url) {
+  // 取得非錨點以及連結的網址
   let links = await page.$$eval("a", (as) =>
     as
       .map((a) => a.href)
@@ -13,11 +14,14 @@ async function checkLinks(page, url) {
           !link.includes("#")
       )
   );
-
+  // 移除專案網址
   links = links.filter((link) => link !== url);
+  // 移除重複的連結
   const uniqueLinks = [...new Set(links)];
 
-  console.log(`共有 ${links.length} 個連結，當中 ${uniqueLinks.length} 個不重複，進行檢查`);
+  console.log(
+    `共有 ${links.length} 個連結，當中 ${uniqueLinks.length} 個不重複，進行檢查`
+  );
 
   const checkLinkBar = new ProgressBar(":bar :current/:total", {
     total: uniqueLinks.length,
@@ -35,9 +39,8 @@ async function checkLinks(page, url) {
       }
     }, link);
 
-    if (status && status.toString().startsWith("4")) {
+    if ((status && status.toString().startsWith("4")) || !status)
       errorLinks.push(link);
-    }
 
     checkLinkBar.tick();
   }
