2ad7772df96a1d6cd040f5614691c9c1e8beea43
Wystan Lin <weijlin850621@gmail.com>
2023-11-26 01:53:26 +0800

    修改提示文字及增加樣式

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // module
 const fs = require("fs");
+const chalk = require('chalk');
 const { launchBrowser } = require("./utils/browser");
 const { handleDialog, handleResponse, handleConsole } = require("./utils/pageUtils");
 const { takeScreenshots } = require("./utils/screenshot");
@@ -8,6 +9,7 @@ const { checkLinks } = require("./utils/linkChecker");
 async function process(url, sizes) {
   const browser = await launchBrowser();
   const page = await browser.newPage();
+  const testPage = await browser.newPage();
 
   await handleDialog(page);
   await handleResponse(page, url);
@@ -15,6 +17,16 @@ async function process(url, sizes) {
   const consoleLog = [];
   await handleConsole(page, consoleLog);
   
+  console.log("=====================================");
+  console.log("任務開始，網址：", url);
+
+  // 設定 cookie
+  await page.setCookie({
+    name: 'temporaryVerifyCode',
+    value: 'ok',
+    domain: url.replace(/^https?:\/\//i, "").split('/')[0] // 假設 URL 是格式正確的
+  });
+
   // 前往網址
   console.log("第一次進入網站");
   await page.goto(url, { waitUntil: "networkidle0" });
@@ -24,7 +36,12 @@ async function process(url, sizes) {
   console.log("已重新整理一次");
 
   // 檢查連結是否正確
-  const errorLinks = await checkLinks(page, url);
+  const errorLinks = await checkLinks(page, testPage, url);
+
+  // 關閉測試頁面
+  await testPage.close();
+  // 切換回 page 
+  await page.bringToFront();
 
   // 截圖
   console.log('開始進行截圖');
@@ -35,24 +52,28 @@ async function process(url, sizes) {
     return getCopy ? getCopy() : 'getCopy() not found';
   });
 
-  console.log("檢查結束");
+  console.log("任務結束");
   console.log("=====================================");
 
   if (errorLinks.length > 0) {
     fs.writeFileSync("errorLinks.txt", errorLinks.join("\n"));
-    console.log(`* 錯誤連結檢查結果： 錯誤連結有 ${errorLinks.length} 個，並已寫入 errorLinks.txt`);
+    console.log(chalk.bgRed(`* 錯誤連結檢查結果： 錯誤連結有 ${errorLinks.length} 個，並已寫入 errorLinks.txt`));
   } else {
-    console.log("* 錯誤連結檢查結果： 沒有錯誤連結");
+    console.log(chalk.bgGreen("* 錯誤連結檢查結果： 沒有錯誤連結"));
   }
 
-  console.log(`* console.log 檢查結果： 共有 ${consoleLog.length} 個 console.log`);
+  if(consoleLog.length > 0)
+    console.log(chalk.bgRed(`* console.log 檢查結果： 共有 ${consoleLog.length} 個 console.log`));
+  else
+    console.log(chalk.bgGreen("* console.log 檢查結果： 沒有 console.log"));
+
   console.log(`* getCopy() 檢查結果： ${copyResult}`);
 
   // 關閉瀏覽器
   await browser.close();
 }
 
-const url = "https://ibestpark2.ito.tw/csfmtw";
+const url = "https://ibestpark2.ito.tw/gia/";
 const sizes = [
   "1920x1080",
   "1680x1050",
